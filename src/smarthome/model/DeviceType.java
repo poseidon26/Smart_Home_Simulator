e0102c8e63f0b05657280c612262727cf84fd8df
Sarp Gürsoy <64532194+poseidon26@users.noreply.github.com>
2025-03-15 20:07:57 +0300

    Add Device model classes

---
+++ src/smarthome/model/DeviceType.java
@@ -0,0 +1,18 @@
+package smarthome.model;
+
+/**
+ * Defines the device types that can be used in the smart home system.
+ */
+public enum DeviceType {
+    THERMOSTAT,
+    LIGHT,
+    SECURITY_SYSTEM,
+    DOOR,
+    WINDOW,
+    AIR_CONDITIONER,
+    TELEVISION,
+    COFFEE_MACHINE,
+    REFRIGERATOR,
+    WASHING_MACHINE,
+    SPEAKER
+}
